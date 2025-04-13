accb950d7a1eec0d395f80f0561d3fd87e50b271
Denys Petryniak <denys.petryniak@gmail.com>
2025-04-13 20:23:10 +0300

    chore: :wrench: install and configure @nuxt/ui module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   future: {
     compatibilityVersion: 4,
   },
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
-})
+  css: ["~/assets/css/main.css"],
+  modules: ["@nuxt/eslint", "@nuxt/ui"],
+});
