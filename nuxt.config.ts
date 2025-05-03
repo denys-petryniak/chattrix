1bc4406b7da0849d4c606498f79c72f75a04346e
Denys Petryniak <denys.petryniak@gmail.com>
2025-05-03 20:48:38 +0300

    feat: :sparkles: add runtimeConfig section with openaiApiKey to nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
+
   future: {
     compatibilityVersion: 4,
   },
+
   devtools: { enabled: true },
+
   css: ["~/assets/css/main.css"],
+
   modules: ["@nuxt/eslint", "@nuxt/ui"],
+
+  runtimeConfig: {
+    openaiApiKey: "",
+  },
 });
