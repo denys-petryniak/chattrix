074373adf354253b7a02b3158582d0b8f3c97aa8
Denys Petryniak <denys.petryniak@gmail.com>
2025-05-05 18:38:04 +0300

    feat: :sparkles: implement AI service with OpenAI and Ollama models, update AI endpoint

--- server/api/ai.ts
+++ server/api/ai.ts
@@ -1,13 +1,25 @@
+import {
+  createOpenAIModel,
+  generateChatResponse,
+} from "../services/ai-service";
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   const { messages } = body;
 
   const id = messages.length.toString();
-  const lastMessage = messages[messages.length - 1];
+
+  const openaiApiKey = useRuntimeConfig().openaiApiKey;
+  const openaiModel = createOpenAIModel(openaiApiKey);
+
+  // you can use Ollama model as well
+  // const ollamaModel = createOllamaModel();
+
+  const response = await generateChatResponse(openaiModel, messages);
 
   return {
     id,
     role: "assistant",
-    content: `(server) You said: ${lastMessage.content}`,
+    content: response,
   };
 });
