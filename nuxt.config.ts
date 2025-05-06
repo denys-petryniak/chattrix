e633656fecb14aa2054db867c5f785e2f523dfe7
Denys Petryniak <denys.petryniak@gmail.com>
2025-05-06 10:33:17 +0300

    feat: :sparkles: install mdc module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
 
   css: ["~/assets/css/main.css"],
 
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/mdc"],
 
   runtimeConfig: {
     openaiApiKey: "",
